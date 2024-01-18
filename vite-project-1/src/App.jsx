7d1184f2ff2cd2e34dab398267bd92b63abe8b79
J Kimball <jeffkimballis@gmail.com>
2024-01-18 09:19:55 -0600

    practice with props, proptypes, and default props

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -1,10 +1,14 @@
-import Card from './Card'
+import Student from './Student'
 
 function App() {
 
   return(
     <>
-      
+      <Student name="Jefferson" age={30} isStudent={true}/>
+      <Student name="Patrick" age={38} isStudent={false}/>
+      <Student name="Donovan" age={28} isStudent={true}/>
+      <Student name="Mary" age={51} isStudent={true}/>
+      <Student name="Larry" />
     </>
   )
 }
